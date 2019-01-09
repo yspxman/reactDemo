d1d6ed2d6538a5f71fa4c274a9808687636e6523
Unknown <yspxman@gmail.com>
2019-01-10 00:35:36 +1100

    add more reduxes

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, FormFeedback} from 'reactstrap';
 import {Link} from 'react-router-dom';
-
+import { Control, LocalForm, Errors} from 'react-redux-form';
 
 class Contact extends Component {
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -17,6 +18,12 @@ const mapStateToProps = state => {
 
     }
 }
+
+const mapDispatchToProps = (dispatch) => {
+   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+};
+
+
 
 class Main extends Component {
 
@@ -42,6 +49,7 @@ class Main extends Component {
         return(
             <DishDetail dish={this.props.dishes.filter(d=>d.id === id)[0]}
                 comments={this.props.comments.filter(c=>c.dishId === id)}
+                addComment = {this.props.addComment}
             />
         );
     }
@@ -61,4 +69,4 @@ class Main extends Component {
   }
 }
 
-export default  withRouter (connect(mapStateToProps)(Main)) ;
+export default  withRouter (connect(mapStateToProps, mapDispatchToProps)(Main)) ;
